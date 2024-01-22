33cf708bf4718744a050347a6de07854de158c30
Pak <pakapongm@gmail.com>
2024-01-22 15:34:21 -0800

    learned about array types

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,10 @@ let course: string = "TypeScript";
 let is_published: boolean = true;
 let level;
 // if variable is unnamed, it is of type any
+// avoid using any type as much as possible, defeats purpose of typescript
+// function render (document: any)
+
+let numbers: number[] = [1, 2, 3];
+// empty array must have type
+// even though array is of type number, it can be assigned to any other type
+

2117a10ac87bd3b83d8c01c7b801bab9553aa050
Pak <pakapongm@gmail.com>
2024-01-22 15:37:40 -0800

    learned about tuples

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,9 @@
 "use strict";
-let age = 20;
-if (age < 50)
-    age += 10;
+let sales = 123456789;
+let course = "TypeScript";
+let is_published = true;
+let level;
+let numbers = [1, 2, 3];
+let user = [1, "Pak"];
+user[0] = 2;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,11 @@ let numbers: number[] = [1, 2, 3];
 // empty array must have type
 // even though array is of type number, it can be assigned to any other type
 
+let user: [number, string] = [1, "Pak"];
+// tuple, fixed length array
+// user.push(1);
+// restrict tuple to 2 elements
+// using push method allows you to store more than 2 elements in tuple but is weird in typescript
+
+
+

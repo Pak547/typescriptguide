5e47fb450a4b45c7558aeaf82830f12b37c0c8dd
Pak <pakapongm@gmail.com>
2024-01-22 15:52:51 -0800

    oops principle in tyepscript

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ console.log(mySize);
 
 // parameter type and return type
 // annotate function with return type
-function calculateTax(income: number, taxYear= 2022): number {
+function calculateTax(income: number, taxYear = 2022): number {
     if (income < 10_000)
         return income * 1.2;
     return income * 1.3; // return type is inferred
@@ -37,3 +37,20 @@ function calculateTax(income: number, taxYear= 2022): number {
 // enable noUnusedLocals in tsconfig.json
 // enable noImplicitReturns in tsconfig.json
 calculateTax(10_000, 2022);
+
+
+// object in type
+let employee: {
+    readonly id: number
+    name?: string
+    retire: (date: Date) => void
+} = {
+    id: 1,
+    name: "",
+    retire: (date: Date) => console.log(date)
+};
+employee.name = "Pak";
+//readonly to avoid changing id
+// ? makes name optional
+
+
